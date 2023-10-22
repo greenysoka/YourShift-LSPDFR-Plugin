10a496ea7fe86d7b50cde627888793da7f797e29
LAXStudios <maximilian@atug.de>
2023-10-22 16:49:36 +0200

    Added services

---
+++ YourShift/Services/ILiteDBService.cs
@@ -0,0 +1,21 @@
+﻿using LiteDB;
+using System.Collections.Generic;
+
+namespace YourShift.Services
+{
+    public interface ILiteDBService<T>
+    {
+        void Save(T entity);
+
+        void Delete(T entity);
+        void Delete(int id);
+
+        void Update(T entity);
+
+        T Get(T entity);
+
+        T Get(int id);
+
+        List<T> GetAll();
+    }
+}
